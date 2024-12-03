5965894cfb777acb7852d6a0f6f9bcda83b106cd
Samuel Freitas <37847692+samfreitasxs@users.noreply.github.com>
2024-12-03 18:08:09 -0300

    include new extensions

---
+++ CryptoSwiftUI/CryptoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoSwiftUIApp.swift
+//  CryptoSwiftUI
+//
+//  Created by Samuel Freitas on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
